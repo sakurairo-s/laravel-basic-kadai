846c9177a881048254a18a90c16563e3f02e3262
sakurairo-s <s_shibazaki_eng@outlook.jp>
2025-05-17 11:11:08 +0900

    課題_クエリビルダを使ってデータを取得しよう

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,11 +1,34 @@
 <!DOCTYPE html>
 <html lang="ja">
-  <head>
+
+<head>
     <meta charset = "UTF-8">
-    <title>Laravel課題</title>
-  </head>
-  <body>
-    <h1>投稿一覧</h1>
-  </body>
+    <title>Laravel課題「クエリビルダを使ってデータを取得しよう」</title>
+</head>
+
+<body>
+    <table>
+      <tr>
+        <!-- ID非表示 -->
+        <!-- <th>ID</th> -->
+        <th>タイトル</th>
+        <th>本文</th>
+        <!-- 作成日時と更新日時は非表示 -->
+        <!-- <th>作成日時</th> -->
+        <!-- <th>更新日時</th> -->
+      </tr>
+      @foreach($posts as $post)
+        <tr>
+            <!-- ID非表示 -->
+            <!-- <td>{{ $post->id }}</td> -->
+            <td>{{ $post->title }}</td>
+            <td>{{ $post->content }}</td>
+            <!-- 作成日時と更新日時は非表示 -->
+            <!-- <td>{{ $post->created_at }}</td> -->
+            <!-- <td>{{ $post->updated_at }}</td> -->
+        </tr>
+      @endforeach
+    </table>
+ </body>
 
   </html>
