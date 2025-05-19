363955edbae38368d85a4603fde400fb58776a6d
sakurairo-s <s_shibazaki_eng@outlook.jp>
2025-05-19 15:33:34 +0900

    課題_データ作成機能とバリデーションを実装しよう

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -17,11 +17,33 @@ class PostController extends Controller
         return view('posts.index', compact('posts'));
     }
 
-public function show($id) {
+    public function show($id) {
         // URL'/posts/{id}'の'{id}'部分と主キー（idカラム）の値が一致するデータをpostsテーブルから取得し、変数$postに代入する
         $post = Post::find($id);
 
         // 変数$productをproducts/show.blade.phpファイルに渡す
         return view('posts.show', compact('post'));
     }
+
+    public function create() {
+        return view('posts.create');
+    }
+
+    public function store(Request $request) {
+        // バリデーションを設定する
+        $request->validate([
+            'title' => 'required|max:20',
+            'content' => 'required|max:200'
+        ]);
+
+        // フォームの入力内容をもとに、テーブルにデータを追加する
+        $post = new Post();
+        $post->title = $request->input('title');
+        $post->content = $request->input('content');
+        $post->save();
+
+        // リダイレクトさせる
+        return redirect("/posts");
+    } 
+
 }
